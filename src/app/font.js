a0524591a17eb9259b044047c2803a57b70fc222
DizzlyD <david4henery00@hotmail.com>
2023-07-24 23:41:28 +0100

    initial-commit

---
+++ src/app/font.js
@@ -0,0 +1,7 @@
+import {Livvic} from "next/font/google"
+
+export const livvic = Livvic({
+  subsets: ["latin"],
+  variable: "--font-livvic",
+  weight: ["400", "500", "600", "700"]
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primaryGreen: "#014E56",
+      primaryRed: "#F67E7E",
+      white: "#ffffff",
+      black: "#002529",
+      secondaryGreen: "#004047",
+      secondaryLightBlue: "#79C8C7",
+      secondaryDarkVeryBlue: "#2C6269",
+      secondaryDarkVeryGreen: "#012F34",
     },
+    fontFamily: {
+      livvic: ["var(--font-livvic)", "san-serif"],
+    },
+    extend: {},
   },
   plugins: [],
-}
+};
